e7aca7f3d7c013206608ef150f8f3025f640e880
Mohammed Mobarak <mohammeda.mobarak@gmail.com>
2025-02-20 05:55:35 +0200

    Verify & reset e1

--- src/Modules/Authentication/Login/Login.jsx
+++ src/Modules/Authentication/Login/Login.jsx
@@ -24,7 +24,7 @@ const navigate = useNavigate();
 
       localStorage.setItem("Token", response.data.token); // Save token on local storge
 
-      toast.success(response?.data?.message || "Login failed!");
+      toast.success(response?.data?.message || "Login Success!");
 
       setTimeout(() => {
         navigate('/dashboard'); // To  "/dashboard" to your target route
